b6b95d71991374f7c96334d12489d4cae5c31c12
cheesis <justus.minx@gmail.com>
2015-07-03 20:35:01 +0200

    more asynch magic

--- test/server.js
+++ test/server.js
@@ -4,6 +4,10 @@ var fs = require('fs');
 var first_page_file_name = 'index.html';  // has to be utf-8
 var serverPort = 8080;
 
+function getDelay(){
+  return parseInt(Math.random()*5000);
+}
+
 // get suffix polyfill; it's from ES6
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
 if (!String.prototype.endsWith) {
@@ -21,29 +25,42 @@ if (!String.prototype.endsWith) {
 http.createServer(function (request, response) {
   console.log(request.method, request.url);
   if(request.method === "POST") {
-    if (request.url === "/baseSoData") {
-      var requestBody = '';
-      request.on('data', function(data) {
-        requestBody += data;
-      });
-      request.on('end', function() {
-
-        var formData = JSON.parse(requestBody);
-        console.log('formData', formData);
-        console.log('opid', formData.opid);
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        var responseData = [
-          {oid:"oim6", avgPrice:50.456, quantity:10000, bs:"S"},
-          {oid:"oim7", avgPrice:50.456, quantity:10000, bs:"S", NBV:1230}
-        ]
+    var requestBody = '';
+    request.on('data', function(data) {
+      requestBody += data;
+    });
+    request.on('end', function() {
+      var postData = JSON.parse(requestBody);
+      console.log('posted data', postData);
+      response.writeHead(200, {'Content-Type': 'application/json'});
+      var responseData = [];
+      if (request.url === "/baseSoData") {
+        postData.forEach(function (orderId) {
+          responseData.push({oid:orderId, avgPrice:50.456, quantity:10000, bs:"S", NBV:1230});
+        });
         setTimeout(function() {
           response.end(JSON.stringify(responseData));
-        }, 6000);
-      });
-    } else {
-      response.writeHead(404, 'Resource Not Found', {'Content-Type': 'text/html'});
-      response.end('<!doctype html><html><head><title>404</title></head><body>404: Resource Not Found</body></html>');
-    }
+        }, getDelay());
+      }
+      else if(request.url === "/vwapBM") {
+        console.log('returning vwap');
+        responseData.push({oid:postData.oid,vwapAvgPrice:50, vwapVolume:50000});
+        setTimeout(function() {
+          response.end(JSON.stringify(responseData));
+        }, getDelay());
+      }
+      else if(request.url === "/customBM") {
+        console.log('returning custom');
+        responseData.push({oid:postData.oid, customAvgPrice:60});
+        setTimeout(function() {
+          response.end(JSON.stringify(responseData));
+        });//, getDelay());
+      }
+      else {
+        response.writeHead(404, 'Resource Not Found', {'Content-Type': 'text/html'});
+        response.end('<!doctype html><html><head><title>404</title></head><body>404: Resource Not Found</body></html>');
+      }
+    });
   } else {
     //don't read the first letter, it's '/', which will make node look at C:\
     var html = fs.readFileSync(request.url.substr(1));
