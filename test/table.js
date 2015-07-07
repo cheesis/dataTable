7482ccdc9ac9c5ceff341f0103c4319e6b60bb55
cheesis <justus.minx@gmail.com>
2015-07-07 21:38:27 +0200

    reindent table.js
    didn't fix everything, find tool for that later

--- test/table.js
+++ test/table.js
@@ -46,180 +46,180 @@ function dataTable(tableId, columns, indexColumn, footer_definition, numberForma
   }
 }
 dataTable.prototype._getRightBorderIndices = function(columns){
-      var right_borders_at = [];
-      columns.map(function (el, i) { // loop through sections
-        return el.columns.length; // builds array with one element per section containing length
-      }).reduce(function (pre, curr) { // cumsum adjusted for indices starting at 0 not 1
-        right_borders_at.push(curr + pre - 1); // minus one because indices start at 0
-        return curr + pre;
-      }, 0);
-      right_borders_at.pop(); // we don't want borders furthest to the right
-      return right_borders_at;
-  }
-  dataTable.prototype._getFlatColumns = function (columns){
-      var _colList = [];
-      columns.forEach(function(sec){
-          sec.columns.forEach(function(c){
-              _colList.push(c.columnName);
-          }, this);
-      }, this);
-      return _colList;
-  };
-  dataTable.prototype._addHeader = function(columns){
-      var topRow   = this._tableRef.createTHead().insertRow();
-      columns.forEach(function(r, i, myArray){
-          var newCell  = topRow.insertCell();
-          newCell.colSpan = r['columns'].length;
-          newCell.appendChild(document.createTextNode(r['section']));
-          newCell.classList.add('table_header_toprow');
-          if (i < columns.length-1) { // no right border furthest to the right
-              newCell.classList.add('border_right');
-          }
-      }, this);
-      var headRow   = this._tableRef.createTHead().insertRow();
-      columns.forEach(function(section){
-          section.columns.forEach(function(c){
-              var newCell  = headRow.insertCell().appendChild(document.createTextNode(c['displayName']));
-          }, this);
-      }, this);
-  };
-
-  dataTable.prototype._setBorders = function(){
-      for (var i = 1, row; row = this._tableRef.rows[i]; i++) { //start from one because the first one has colspan, borders are set differently
-          for (var j = 0, col; col = row.cells[j]; j++) {
-              if (this._rightBordersAt.indexOf(j) > -1) {
-                  col.classList.add("border_right");
-              }
-          }
-      }
-  };
-
-  dataTable.prototype._addFooter = function(){
-      var footRow   = this._tableRef.createTFoot().insertRow();
-      //same code as in tbody -- TODO
-      this._flatColumns.forEach(function(r){
-          var newCell  = footRow.insertCell();
-          newCell.dataset.columnName = r;
-      }, this);
-  };
-
-  dataTable.prototype._formatData = function (col, d) {
-    if (col.hasOwnProperty('dataFormater')) {
-      console.log('dataFormater on column found');
-      return col.dataFormater(d);
+  var right_borders_at = [];
+  columns.map(function (el, i) { // loop through sections
+    return el.columns.length; // builds array with one element per section containing length
+  }).reduce(function (pre, curr) { // cumsum adjusted for indices starting at 0 not 1
+    right_borders_at.push(curr + pre - 1); // minus one because indices start at 0
+    return curr + pre;
+  }, 0);
+  right_borders_at.pop(); // we don't want borders furthest to the right
+  return right_borders_at;
+}
+dataTable.prototype._getFlatColumns = function (columns){
+  var _colList = [];
+  columns.forEach(function(sec){
+    sec.columns.forEach(function(c){
+        _colList.push(c.columnName);
+    }, this);
+  }, this);
+  return _colList;
+}
+dataTable.prototype._addHeader = function(columns){
+  var topRow   = this._tableRef.createTHead().insertRow();
+  columns.forEach(function(r, i, myArray){
+    var newCell  = topRow.insertCell();
+    newCell.colSpan = r['columns'].length;
+    newCell.appendChild(document.createTextNode(r['section']));
+    newCell.classList.add('table_header_toprow');
+    if (i < columns.length-1) { // no right border furthest to the right
+        newCell.classList.add('border_right');
     }
-    else if (this._dataFormatter)
-      return this._dataFormatter(d)
-    else
-      return d;
-    // return this._dataFormatter ? this._dataFormatter(d) : d;
-  };
+  }, this);
+  var headRow   = this._tableRef.createTHead().insertRow();
+  columns.forEach(function(section){
+    section.columns.forEach(function(c){
+        var newCell  = headRow.insertCell().appendChild(document.createTextNode(c['displayName']));
+    }, this);
+  }, this);
+}
 
-  dataTable.prototype._populateFooter = function(){
-      var footRow = this._tableRef.createTFoot().rows[0];
-      this._footerDefinition.forEach(function(d,i){
-          var where = this._flatColumns.indexOf(d.columnName);
+dataTable.prototype._setBorders = function(){
+  for (var i = 1, row; row = this._tableRef.rows[i]; i++) { //start from one because the first one has colspan, borders are set differently
+    for (var j = 0, col; col = row.cells[j]; j++) {
+      if (this._rightBordersAt.indexOf(j) > -1) {
+          col.classList.add("border_right");
+      }
+    }
+  }
+}
+
+dataTable.prototype._addFooter = function(){
+  var footRow   = this._tableRef.createTFoot().insertRow();
+  //same code as in tbody -- TODO
+  this._flatColumns.forEach(function(r){
+    var newCell  = footRow.insertCell();
+    newCell.dataset.columnName = r;
+  }, this);
+}
+
+dataTable.prototype._formatData = function (col, d) {
+  if (col.hasOwnProperty('dataFormater')) {
+    console.log('dataFormater on column found');
+    return col.dataFormater(d);
+  }
+  else if (this._dataFormatter)
+    return this._dataFormatter(d)
+  else
+    return d;
+  // return this._dataFormatter ? this._dataFormatter(d) : d;
+}
+
+dataTable.prototype._populateFooter = function(){
+    var footRow = this._tableRef.createTFoot().rows[0];
+    this._footerDefinition.forEach(function(d,i){
+        var where = this._flatColumns.indexOf(d.columnName);
+        var things = {
+          getColInFootRow: dataTableSelector.colInFootRowSelector.bind(this, this._tableId),
+          getColInRow: dataTableSelector.colInFootRowSelector.bind(this, this._tableId),
+          getColInBodyRow: dataTableSelector.colInBodyRowSelector.bind(this, this._tableId),
+          getCol: dataTableSelector.columnSelector.bind(this, this._tableId)
+        };
+        var calculatedValue = d.calc(things);
+        footRow.cells[where].innerHTML = this._formatData(d, calculatedValue);
+        footRow.cells[where].__data__ = calculatedValue;
+    }, this);
+}
+dataTable.prototype.populate = function(data){
+  //first we handle the data that we get directly from the source
+  var tbody = new Object();
+  if (this._tableRef.tBodies.length == 0) {
+      tbody = this._tableRef.appendChild(document.createElement('tbody'));
+  } else {
+      tbody = this._tableRef.tBodies[0];
+  }
+
+  var bodyrows = tbody.rows,
+      rowIds = []; //this array contains the data attribute id of each row
+  for (i = 0; i < bodyrows.length; i++) {
+      rowIds.push(bodyrows[i].dataset.id);
+  }
+
+  //get row that contains the id
+  data.forEach(function(d,i){
+      var id = d[this._rowIndex];
+      var rowNbr = rowIds.indexOf(id);
+      if (rowNbr == -1){
+          //create
+          var bodyRow = tbody.insertRow();
+          bodyRow.dataset.id = id;
+          this._columns.forEach(function(section){
+            section.columns.forEach(function(column){
+                var newCell  = bodyRow.insertCell();
+                newCell.appendChild(document.createTextNode(this._formatData(column, d[column.columnName]))); //e.g. d.oid here written as d['oid']
+                newCell.dataset.columnName = column.columnName;
+                newCell.__data__ = d[column.columnName];
+                newCell.setAttribute("contentEditable", true);
+                theTableInstance = this; // save for later use, 'this' will be something else
+                newCell.addEventListener("blur", function () {
+                  this.__data__ = this.innerHTML;
+                  this.innerHTML = theTableInstance._formatData(column, this.__data__);
+                  theTableInstance.populate([]); //recalculate values
+                });
+                newCell.addEventListener("focus", function () {
+                  this.innerHTML = this.__data__;
+                });
+            }, this);
+          }, this);
+      } else {
+          //update row
+          Object.getOwnPropertyNames(d).forEach(function(prop, pindex){
+              if (prop != this._rowIndex) //we don't want to update the index
+              {
+                this._columns.forEach(function(section){
+                  section.columns.forEach(function(column){
+                    if (d.hasOwnProperty(column.columnName)){
+                      var columnIndex = this._flatColumns.indexOf(column.columnName);
+                      bodyrows[rowNbr].cells[columnIndex].innerHTML = this._formatData(column, d[column.columnName]);
+                      bodyrows[rowNbr].cells[columnIndex].__data__ = d[column.columnName];
+                    }
+                  }, this);
+                }, this);
+              }
+          }, this);
+      }
+  }, this); //yes,very confusing. if we don't pass what 'this' should be then it will be the global object aka Window
+            // thank you javascript
+
+  //compute values for calculated columns
+  //almost same code as _populateFooter - bad style
+  this._columns.forEach(function(section){
+    section.columns.forEach(function(column){
+      if (column.hasOwnProperty('calc')){
+        var columnIndex = this._flatColumns.indexOf(column.columnName);
+        for (i = 0; i < bodyrows.length; i++) {
           var things = {
             getColInFootRow: dataTableSelector.colInFootRowSelector.bind(this, this._tableId),
-            getColInRow: dataTableSelector.colInFootRowSelector.bind(this, this._tableId),
+            getColInRow: dataTableSelector.colInBodyRowSelector.bind(this, this._tableId, bodyrows[i].dataset.id),
             getColInBodyRow: dataTableSelector.colInBodyRowSelector.bind(this, this._tableId),
             getCol: dataTableSelector.columnSelector.bind(this, this._tableId)
           };
-          var calculatedValue = d.calc(things);
-          footRow.cells[where].innerHTML = this._formatData(d, calculatedValue);
-          footRow.cells[where].__data__ = calculatedValue;
-      }, this);
-  };
-  dataTable.prototype.populate = function(data){
-      //first we handle the data that we get directly from the source
-      var tbody = new Object();
-      if (this._tableRef.tBodies.length == 0) {
-          tbody = this._tableRef.appendChild(document.createElement('tbody'));
-      } else {
-          tbody = this._tableRef.tBodies[0];
+          var calculatedValue = column.calc(things);
+          bodyrows[i].cells[columnIndex].innerHTML = this._formatData(column, calculatedValue);
+          bodyrows[i].cells[columnIndex].__data__ = calculatedValue;
+        }
       }
-
-      var bodyrows = tbody.rows,
-          rowIds = []; //this array contains the data attribute id of each row
-      for (i = 0; i < bodyrows.length; i++) {
-          rowIds.push(bodyrows[i].dataset.id);
-      }
-
-      //get row that contains the id
-      data.forEach(function(d,i){
-          var id = d[this._rowIndex];
-          var rowNbr = rowIds.indexOf(id);
-          if (rowNbr == -1){
-              //create
-              var bodyRow = tbody.insertRow();
-              bodyRow.dataset.id = id;
-              this._columns.forEach(function(section){
-                section.columns.forEach(function(column){
-                    var newCell  = bodyRow.insertCell();
-                    newCell.appendChild(document.createTextNode(this._formatData(column, d[column.columnName]))); //e.g. d.oid here written as d['oid']
-                    newCell.dataset.columnName = column.columnName;
-                    newCell.__data__ = d[column.columnName];
-                    newCell.setAttribute("contentEditable", true);
-                    theTableInstance = this; // save for later use, 'this' will be something else
-                    newCell.addEventListener("blur", function () {
-                      this.__data__ = this.innerHTML;
-                      this.innerHTML = theTableInstance._formatData(column, this.__data__);
-                      theTableInstance.populate([]); //recalculate values
-                    });
-                    newCell.addEventListener("focus", function () {
-                      this.innerHTML = this.__data__;
-                    });
-                }, this);
-              }, this);
-          } else {
-              //update row
-              Object.getOwnPropertyNames(d).forEach(function(prop, pindex){
-                  if (prop != this._rowIndex) //we don't want to update the index
-                  {
-                    this._columns.forEach(function(section){
-                      section.columns.forEach(function(column){
-                        if (d.hasOwnProperty(column.columnName)){
-                          var columnIndex = this._flatColumns.indexOf(column.columnName);
-                          bodyrows[rowNbr].cells[columnIndex].innerHTML = this._formatData(column, d[column.columnName]);
-                          bodyrows[rowNbr].cells[columnIndex].__data__ = d[column.columnName];
-                        }
-                      }, this);
-                    }, this);
-                  }
-              }, this);
-          }
-      }, this); //yes,very confusing. if we don't pass what 'this' should be then it will be the global object aka Window
-                // thank you javascript
-
-      //compute values for calculated columns
-      //almost same code as _populateFooter - bad style
-      this._columns.forEach(function(section){
-        section.columns.forEach(function(column){
-          if (column.hasOwnProperty('calc')){
-            var columnIndex = this._flatColumns.indexOf(column.columnName);
-            for (i = 0; i < bodyrows.length; i++) {
-              var things = {
-                getColInFootRow: dataTableSelector.colInFootRowSelector.bind(this, this._tableId),
-                getColInRow: dataTableSelector.colInBodyRowSelector.bind(this, this._tableId, bodyrows[i].dataset.id),
-                getColInBodyRow: dataTableSelector.colInBodyRowSelector.bind(this, this._tableId),
-                getCol: dataTableSelector.columnSelector.bind(this, this._tableId)
-              };
-              var calculatedValue = column.calc(things);
-              bodyrows[i].cells[columnIndex].innerHTML = this._formatData(column, calculatedValue);
-              bodyrows[i].cells[columnIndex].__data__ = calculatedValue;
-            }
-          }
-        }, this);
-      }, this);
+    }, this);
+  }, this);
 
 
-      // rest
-      this._populateFooter();
-      this._setBorders();
-      this._tableRef.dispatchEvent(this.populateEvent);
+  // rest
+  this._populateFooter();
+  this._setBorders();
+  this._tableRef.dispatchEvent(this.populateEvent);
 
-      return data; //return the data so we can use it in a promise.then() method
-  }
+  return data; //return the data so we can use it in a promise.then() method
+}
 
 //Singleton
 var dataTableSelector =
